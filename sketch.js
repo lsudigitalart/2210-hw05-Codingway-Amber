469a69069b7973effa1fceabc46f653d89ab868e
Codingway-Amber <amberreine7@gmail.com>
2024-10-07 12:58:56 -0500

    added rotation for rectangle

--- sketch.js
+++ sketch.js
@@ -1,21 +1,30 @@
+var x = 0;
+
+
 function setup() {
   createCanvas(800, 800); 
-  
+  rectMode(CENTER); 
+  frameRate(28);
 }
 
 function draw() {
   background(240, 239, 224); 
   // mouse position
-    // fill(255, 0, 0); 
-    // stroke(255, 0, 0);
-    text(mouseX, 0, 180); 
-    text(mouseY, 0, 200); 
-  
-  noFill(); 
-  stroke(0)
-  square(100, 100, 600);
+  // fill(255, 0, 0); 
+  // stroke(255, 0, 0);
+  text(mouseX, 0, 180); 
+  text(mouseY, 0, 200); 
 
-  stroke(255, 0, 0)
+  push();
+  x+= 0.02;
+  translate (width/2, height/2); 
+  noFill(); 
+  stroke(2);
+  rotate(x);
+  rect(0, 0, 600, 600);
+  pop();
+
+  stroke(255, 0, 0);
   line(170, 170, 630, 630); 
   line(630, 170, 170, 630);
 }
