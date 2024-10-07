7709b6bff55dbe49f441b47bce7a2169737c616b
Codingway-Amber <amberreine7@gmail.com>
2024-10-06 19:47:53 -0500

    made the wall drawing 160

--- sketch.js
+++ sketch.js
@@ -1,7 +1,21 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 800); 
+  
 }
 
 function draw() {
-  background(220);
+  background(240, 239, 224); 
+  // mouse position
+    // fill(255, 0, 0); 
+    // stroke(255, 0, 0);
+    text(mouseX, 0, 180); 
+    text(mouseY, 0, 200); 
+  
+  noFill(); 
+  stroke(0)
+  square(100, 100, 600);
+
+  stroke(255, 0, 0)
+  line(170, 170, 630, 630); 
+  line(630, 170, 170, 630);
 }
